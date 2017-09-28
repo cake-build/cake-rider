14be1fcbf8eeb47bea2e2a000d90adf7404c012f
Matt Ellis <m.t.ellis@gmail.com>
2017-09-28 23:49:08 +0100

    Disable build project before run for Cake tasks

--- rider/src/main/kotlin/net/cakebuild/run/CakeConfigurationFactory.kt
+++ rider/src/main/kotlin/net/cakebuild/run/CakeConfigurationFactory.kt
@@ -1,6 +1,10 @@
 package net.cakebuild.run
 
+import com.intellij.execution.BeforeRunTask
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Key
+import com.jetbrains.rider.build.tasks.BuildProjectBeforeRunTask
+import com.jetbrains.rider.build.tasks.BuildProjectBeforeRunTaskProvider
 import com.jetbrains.rider.run.configurations.DotNetConfigurationFactoryBase
 import com.jetbrains.rider.runtime.RiderDotNetActiveRuntimeHost
 import com.jetbrains.rider.util.idea.getComponent
@@ -13,5 +17,11 @@ class CakeConfigurationFactory(cakeConfigurationType: CakeConfigurationType)
     override fun createTemplateConfiguration(project: Project): CakeConfiguration {
         val host = project.getComponent<RiderDotNetActiveRuntimeHost>()
         return CakeConfiguration(project, this, host)
+    }
+
+    override fun configureBeforeRunTaskDefaults(providerID: Key<out BeforeRunTask<BeforeRunTask<*>>>?, task: BeforeRunTask<out BeforeRunTask<*>>?) {
+        if (providerID == BuildProjectBeforeRunTaskProvider.buildId && task is BuildProjectBeforeRunTask) {
+            task.isEnabled = false
+        }
     }
 }
