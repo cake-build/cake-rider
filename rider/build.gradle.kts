fe4503337f7d7e4a7acfb77f4f44ac6fe1a0fe46
Nils Andresen <nils@nils-andresen.de>
2021-04-02 23:10:08 +0200

    (GH-77) dropped support for RD-2019.3

--- rider/build.gradle.kts
+++ rider/build.gradle.kts
@@ -95,11 +95,13 @@ tasks {
     getByName<JavaExec>("buildSearchableOptions") {
         jvmArgs(
             "--illegal-access=deny",
-            "--add-opens=java.desktop/sun.awt=ALL-UNNAMED",
-            "--add-opens=java.desktop/java.awt=ALL-UNNAMED",
             "--add-opens=java.base/java.lang=ALL-UNNAMED",
+            "--add-opens=java.base/java.util=ALL-UNNAMED",
+            "--add-opens=java.desktop/java.awt=ALL-UNNAMED",
+            "--add-opens=java.desktop/java.awt.event=ALL-UNNAMED",
             "--add-opens=java.desktop/javax.swing=ALL-UNNAMED",
             "--add-opens=java.desktop/javax.swing.plaf.basic=ALL-UNNAMED",
+            "--add-opens=java.desktop/sun.awt=ALL-UNNAMED",
             "--add-opens=java.desktop/sun.font=ALL-UNNAMED",
             "--add-opens=java.desktop/sun.swing=ALL-UNNAMED"
         )
