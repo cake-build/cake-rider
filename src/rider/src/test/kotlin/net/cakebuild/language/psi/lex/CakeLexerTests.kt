48bf822fec83e3cb77091b4abc091ebc29ce67ad
Nils Andresen <nils@nils-andresen.de>
2023-12-20 22:08:06 +0100

    (maint) added some ktlint settings
    and added the last commit to .git-blame-ignore-revs.

--- src/rider/src/test/kotlin/net/cakebuild/language/psi/lex/CakeLexerTests.kt
+++ src/rider/src/test/kotlin/net/cakebuild/language/psi/lex/CakeLexerTests.kt
@@ -5,14 +5,19 @@ import net.cakebuild.language.psi.CakeLexerAdapter
 
 /*
  This class extends a jUnit.framework.testCase
- hence, to NOT use @Test annotations here,
+ hence, do NOT use @Test annotations here,
  and DO name all tests starting with test. (i.e. testSimple())
+
+ Also, ktlint normally recognizes tests and allows having backtick-function names.
+ However, due to the missing test-annotations, it does not. Hence the Suppression of
+ the "function-naming" rule in each function.
  */
 class CakeLexerTests : LexerTestCase() {
     override fun createLexer() = CakeLexerAdapter()
 
     override fun getDirPath() = "src/test/testData/net/cakebuild/language/psi/"
 
+    @Suppress("ktlint:standard:function-naming")
     fun `test unknown word`() {
         doTest(
             "someWord",
@@ -29,10 +34,12 @@ class CakeLexerTests : LexerTestCase() {
         )
     }
 
+    @Suppress("ktlint:standard:function-naming")
     fun `test eol-comment`() {
         doTest("// foo bar baz", "CakeTokenType.EOL_COMMENT ('// foo bar baz')")
     }
 
+    @Suppress("ktlint:standard:function-naming")
     fun `test multi-line eol comment`() {
         val input =
             """
@@ -51,6 +58,7 @@ class CakeLexerTests : LexerTestCase() {
         doTest(input, expected)
     }
 
+    @Suppress("ktlint:standard:function-naming")
     fun `test multi-line block comment`() {
         val input =
             """
@@ -70,6 +78,7 @@ class CakeLexerTests : LexerTestCase() {
         doTest(input, expected)
     }
 
+    @Suppress("ktlint:standard:function-naming")
     fun `test multi-empty lines`() {
         val input =
             """
@@ -90,6 +99,7 @@ class CakeLexerTests : LexerTestCase() {
         doTest(input, expected)
     }
 
+    @Suppress("ktlint:standard:function-naming")
     fun `test task`() {
         val input = "Task(\"Default\")"
         val expected =
